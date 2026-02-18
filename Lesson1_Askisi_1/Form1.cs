13e8d328aeb737d5c26b7aac8d86fb7372080071
Panos <p.kontozidis@yahoo.gr>
2026-02-18 15:38:14 +0200

    Form Deploy

--- Lesson1_Askisi_1/Form1.cs
+++ Lesson1_Askisi_1/Form1.cs
@@ -6,5 +6,23 @@ namespace Lesson1_Askisi_1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNum1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int sum;
+            sum = Convert.ToInt32(txtNum1.Text) + Convert.ToInt32(txtNum2.Text);
+
+            txtNum3.Text=sum.ToString();
+        }
     }
 }
